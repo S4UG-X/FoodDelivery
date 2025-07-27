1bb9479307212dbd709ba0fca572ab5a496e0a29
saugat <saugatlamichhane77@gmail.com>
2025-07-27 18:51:36 -0500

    AppDownload Section Completed

--- frontend/src/components/AppDownload.jsx
+++ frontend/src/components/AppDownload.jsx
@@ -3,13 +3,13 @@ import images from '../assets/asset'
 
 const AppDownload = () => {
   return (
-    <div className="m-auto mt-25 text-3xl text-center  font-medium">
+    <div className="m-auto mt-25 text-4xl text-center  font-medium">
       <p>
         Better Experience Download <br /> Zomato App
       </p>
       <div className="flex justify-center gap-3 mt-10">
-        <img src={images.appStore} alt="" />
-        <img src={images.googleStore} alt="" />
+        <img className='cursor-pointer duration-200 hover:scale-105' src={images.appStore} alt="" />
+        <img className='cursor-pointer duration-200 hover:scale-105' src={images.googleStore} alt="" />
       </div>
     </div>
   );
